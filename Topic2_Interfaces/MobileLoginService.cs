1f701b4d4a13c268d20d6fb08555570a9321b959
Oleg S. <kindovso@gmail.com>
2025-12-02 20:49:47 +0100

    Add MobileLoginService and Scenario 3 for mobile testing
    A new `MobileLoginService` class was added to the `Topic2_Interfaces` namespace, implementing the `ILoginService` interface. This class simulates a mobile login process with detailed logging and updates the `IsLoggedIn` property upon successful login.
    
    In `Program.cs`, Scenario 3 was introduced to demonstrate the use of `MobileLoginService`. The scenario integrates with the existing `LoginTest` class, showcasing its compatibility with the new implementation without requiring changes to the test code.
    
    Additionally, a `Console.WriteLine` statement was added to improve output readability by separating Scenario 2 and Scenario 3.

---
+++ Topic2_Interfaces/MobileLoginService.cs
@@ -0,0 +1,18 @@
+﻿namespace Topic2_Interfaces
+{
+    public class MobileLoginService : ILoginService
+    {
+        public bool IsLoggedIn { get; private set; }
+
+        public void Login(string username, string password)
+        {
+            Console.WriteLine("[Mobile Driver] Открываю браузер...");
+            Console.WriteLine($"[Mobile Driver] Ищу поле Username, ввожу '{username}'");
+            Console.WriteLine($"[Mobile Driver] Ищу поле Password, ввожу '***'");
+            Console.WriteLine("[Mobile Driver] Кликаю кнопку 'Войти'");
+
+            IsLoggedIn = true;
+            Console.WriteLine("[Mobile Driver] Успешный вход через интерфейс сайта.");
+        }
+    }
+}
--- Topic2_Interfaces/Program.cs
+++ Topic2_Interfaces/Program.cs
@@ -15,3 +15,12 @@ ILoginService apiService = new ApiLoginService();
 // Тот же самый класс теста работает с новой реализацией без изменений кода теста!
 LoginTest test2 = new LoginTest(apiService);
 test2.Run();
+
+Console.WriteLine("\n--------------------------------------\n");
+
+Console.WriteLine("Сценарий 3: Мы хотим прогнать Мобильный тест (как делает юзер на смартфоне)");
+// Создаем конкретную реализацию API
+ILoginService mobileService = new MobileLoginService();
+// Тот же самый класс теста работает с новой реализацией без изменений кода теста!
+LoginTest test3 = new LoginTest(mobileService);
+test3.Run();
